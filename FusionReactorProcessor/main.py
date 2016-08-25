a984ca2e4317cd01550e34de6a0e76b3f9c95ae1
kangli914 <likang914@hotmail.com>
2016-08-24 20:39:07 -0400

    add set

--- FusionReactorProcessor/main.py
+++ FusionReactorProcessor/main.py
@@ -1,15 +1,17 @@
 if __name__ == "__main__":
     # Initialize a names dictionary as empty to start with.
     # Each key in this dictionary will be a name and the value
-    # will be the number of times that name appears.
     names = {}
     observations_file = open('request.log')
     for line in observations_file:
-            #print(line)
             strline = line.strip()
             k,v = strline.split(' ')
             if k not in names:
-                names[k] = v
+                s = set()
+                s.add(v)
+                names[k] = s
+            else:
+                names[k].add(v)
 
     observations_file.close()
 
