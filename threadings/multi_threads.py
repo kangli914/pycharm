582026563ef889f7067c3bdf424f5170e8eb29a4
kangli <kli@company.com>
2020-09-20 10:58:51 -0400

    update time format for multi thread

--- threadings/multi_threads.py
+++ threadings/multi_threads.py
@@ -1,22 +1,24 @@
 import logging
 import threading
 import time
+
 """
-[10:24:46]: Main    : create and start Thread-1.
-[10:24:46]: Thread-1: starting
-[10:24:46]: Main    : create and start Thread-2.
-[10:24:46]: Thread-2: starting
-[10:24:46]: Main    : create and start Thread-3.
-[10:24:46]: Thread-3: starting
-[10:24:46]: Main    : before joining Thread-1
-[10:24:56]: Thread-2: finishing
-[10:24:56]: Thread-3: finishing
-[10:24:56]: Thread-1: finishing
-[10:24:56]: Main    : Thread-1 done
-[10:24:56]: Main    : before joining Thread-2
-[10:24:56]: Main    : Thread-2 done
-[10:24:56]: Main    : before joining Thread-3
-[10:24:56]: Main    : Thread-3 done
+[2020-09-20 10:57:22]: [INFO]: Main    : create and start Thread-1
+[2020-09-20 10:57:22]: [INFO]: Thread-1: starting
+[2020-09-20 10:57:22]: [INFO]: Main    : create and start Thread-2
+[2020-09-20 10:57:22]: [INFO]: Thread-2: starting
+[2020-09-20 10:57:22]: [INFO]: Main    : create and start Thread-3
+[2020-09-20 10:57:22]: [INFO]: Thread-3: starting
+[2020-09-20 10:57:22]: [INFO]: Main    : before joining Thread-1
+[2020-09-20 10:57:32]: [INFO]: Thread-3: finishing
+[2020-09-20 10:57:32]: [INFO]: Thread-2: finishing
+[2020-09-20 10:57:32]: [INFO]: Thread-1: finishing
+[2020-09-20 10:57:32]: [INFO]: Main    : Thread-1 done
+[2020-09-20 10:57:32]: [INFO]: Main    : before joining Thread-2
+[2020-09-20 10:57:32]: [INFO]: Main    : Thread-2 done
+[2020-09-20 10:57:32]: [INFO]: Main    : before joining Thread-3
+[2020-09-20 10:57:32]: [INFO]: Main    : Thread-3 done
+Finished in 10.01 seoncds
 """
 
 
@@ -26,8 +28,9 @@ def thread_function(name):
     logging.info("Thread-%s: finishing", name)
 
 if __name__ == "__main__":
-    format = "[%(asctime)s]: %(message)s"
-    logging.basicConfig(format=format, level=logging.INFO, datefmt="%H:%M:%S")
+
+    format = "[%(asctime)s]: [%(levelname)s]: %(message)s"
+    logging.basicConfig(format=format, level=logging.INFO, datefmt="%Y-%m-%d %H:%M:%S")
 
     threads = []
     # for index in range(3):
@@ -41,6 +44,8 @@ if __name__ == "__main__":
     #     thread.join()
     #     logging.info("Main    : thread %d done", index)
 
+    start = time.perf_counter()
+
     for index in range(3):
         logging.info("Main    : create and start Thread-%d", index+1)
         t = threading.Thread(target=thread_function, args=(index+1,))
@@ -51,3 +56,6 @@ if __name__ == "__main__":
         logging.info("Main    : before joining %s", thread.getName())
         thread.join()
         logging.info("Main    : %s done", thread.getName())
+
+    finish = time.perf_counter()
+    print(f"Finished in {round(finish-start, 2)} seoncds")
