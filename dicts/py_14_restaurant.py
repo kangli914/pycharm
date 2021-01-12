d529a218ecc521c2d739f1ca3d322048c1bd5216
kangli <kang.li@elasticpath.com>
2021-01-12 09:52:01 -0500

    dict restaurant

--- dicts/py_14_restaurant.py
+++ dicts/py_14_restaurant.py
@@ -2,18 +2,27 @@
 
 MENU = dict(tea=5, sandwich=10, burger=20)
 
-total = 0
-order = input("Entry Order: ")
-# price = dict(order, None)
-price = MENU.get(order, None)
 
-while True:
-    # if order is None:
-    if order == "":
-        break
+def restaurant():
+    total = 0
 
-    if price is not None:
-        total += price
-        print(f"{order} costs {price}, total is {total}")
-    else:
-        print("you order something not from the Menu")
+    while True:
+
+        order = input("Entry Order: ").strip()
+
+        ## if "order" is an empty string, break out of the loop 
+        # if order == "":
+        if not order:               # python conventions
+            print(f"total is {total}")
+            break
+
+        price = MENU.get(order, None)
+        if price is not None:
+        # if order in MENU:
+            total += price
+            print(f"{order} costs {price}, total is {total}")
+        else:
+            print("you order something not from the Menu")
+
+
+restaurant()
