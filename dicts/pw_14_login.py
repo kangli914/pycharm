8413c08a84102eae8c65bb819d49f39515e75e10
kangli <li9kang14@gmail.com>
2021-01-14 09:06:49 -0500

    add workout 14 login

---
+++ dicts/pw_14_login.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+'''
+DATABASE = dict(kangli="kangli", perfeng="perfeng")
+
+while True:
+    user = input("user name: ")
+
+    password = DATABASE.get(user, None)
+    if password is None:
+        print("user not exist")
+    else:
+        pwd = input("password: ")
+        if (pwd == password):
+            print("user successfully login")
+            break
+        else:
+            print(f"user {user} password not match")
+'''
+
+user_list = [("kangli", "kangli"), ("perfeng", "perfeng")]
+DATABASE = dict(user_list)
+
+while True:
+    user = input("user name: ").strip()
+    if user in DATABASE:
+        pwd = input("password: ").strip()
+        if pwd == DATABASE[user]:
+            print("user successfully login")
+            break
+        else:
+            print(f"{user} password not match")
+    else:
+        print("user not exit")
