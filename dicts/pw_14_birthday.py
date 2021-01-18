8f07d1ecd0c61d85249c0a50d8f4aa1ccf0a0726
kangli <kang.li@elasticpath.com>
2021-01-18 10:24:23 -0500

    add pw 14 birthday

---
+++ dicts/pw_14_birthday.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+from datetime import date
+
+birth_dates = dict([
+    ("david", date(1970, 10, 1)),
+    ("max", date(2008, 10, 5))])
+
+
+while True:
+    name = input("name: ").strip().lower()
+    date_value = birth_dates.get(name)
+
+    # exit when empty string - python conventions for checking empty 
+    if not name:
+        print("exit...")
+        break
+
+    # value is None
+    if not date_value:
+        print(f"person {name} does not exist in the database")
+    else:
+        today = date.today()
+        print(f"{name} was born on {date_value.isoformat()} and is {(today - date_value).days} days old")
