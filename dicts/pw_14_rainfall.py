2314f69a630ad2bfab8694a9459d187c86993858
kangli <kang.li@elasticpath.com>
2021-01-26 09:23:53 -0500

    add default dict impl

--- dicts/pw_14_rainfall.py
+++ dicts/pw_14_rainfall.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python3
 
+from collections import defaultdict
+
 """ print out city rainfall """
 
+
 city_rainfall = dict()
+city_rainfall_default = defaultdict(int)
 
 
 def get_rainfall():
-
+    """Use reguler dictionary."""
     while True:
         city = input("Enter City>: ").strip().capitalize()
 
@@ -21,4 +25,23 @@ def get_rainfall():
             city_rainfall[city] = city_rainfall.get(city, 0) + rain
 
 
-get_rainfall()
+def get_rainfall_default():
+    """Use defaultdict dictionary from the collection."""
+    while True:
+        city = input("Enter City>: ").strip().capitalize()
+
+        if not city:
+            break
+        else:
+            rain = int(input("Entry Rainfall>: "))
+            # city_rainfall_default[city] = city_rainfall_default[city] + rain
+            city_rainfall_default[city] += rain
+
+    for k, v in city_rainfall_default.items():
+        print(f"{k}: {v} millimeters")
+
+
+# get_rainfall()
+
+
+get_rainfall_default()
