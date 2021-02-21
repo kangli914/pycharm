9ad36c21f1ab344a0f14701e878c385cb66220f0
kangli <li9kang14@gmail.com>
2021-02-21 13:37:22 -0500

    add set unique file extension in a directory

---
+++ dicts/pw_17_set_unique-file-suffix.py
@@ -0,0 +1,20 @@
+#!/usr/bin/eny python3
+
+"""Find directory file extensions(suffixes)."""
+
+import os
+
+dir_path = os.getcwd()
+ext_set = set()
+
+for item in os.listdir(f"{dir_path}/dicts"):
+
+    ## split() split dict and file to a tuple pair
+    # print(os.path.split(f"{dir_path}/dicts/{item}"))
+
+    ## split() split dict/file_name and file extension to a tuple pair
+    # print(os.path.splitext(f"{dir_path}/dicts/{item}"))
+
+    ext_set.add(os.path.splitext(item)[1])
+
+print(ext_set)
