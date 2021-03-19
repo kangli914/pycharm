9c3af05d701718a051363426d1aad302a163fa8f
kangli <kang.li@elasticpath.com>
2021-03-19 09:45:49 -0400

    add complete for mysum bigger code

--- lists/ex10_mysum_bigger.py
+++ lists/ex10_mysum_bigger.py
@@ -5,10 +5,9 @@
 That argument indicates the threshold for including an argument in the sum.
 """
 
-
 def mysum_bigger_than(base, *args):
 
-    ## having problem with type: TypeError: unsupported operand type(s) for +=: 'int' and 'str': sum += element
+    ## having problem with type: TypeError: unsupported operand type(s) for +=: 'int' and 'str': sum += element for string case:  mysum_bigger_than("c", "a", "b", "c", "d", "e")
     # sum = 0
     # for element in args[:]:
     #     if element >= base:
@@ -17,19 +16,22 @@ def mysum_bigger_than(base, *args):
     # print(sum)
 
     first = args[0]
-    # if first < base:
-    #     new_args = args[1:]
-    #     print(args)
-    #     first = args[1]
-    # if first < base:
-    #     first = first - first
+    first_idx = 0
+
+    # find the first idx and first item which is bigger than base value
+    for idx, item in enumerate(args):
+        if item >= base:
+            first_idx = idx
+            first = item
+            break
 
     # print(first)
-    for element in args[1:]:
+    # print(args[first_idx+1:])
+    for element in args[first_idx + 1:]:
         if element >= base:
             first += element
     print(first)
 
 
-mysum_bigger_than(10, 5, 20, 30, 6)
-mysum_bigger_than("c", "a", "b", "c", "d", "e")
+mysum_bigger_than(30, 5, 20, 30, 6)
+mysum_bigger_than("a", "a", "b", "c", "d", "e")
