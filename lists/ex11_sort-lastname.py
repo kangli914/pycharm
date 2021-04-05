5b0a43a4fa33c0d7ca05f9312448081b21115e46
kangli <kang.li@elasticpath.com>
2021-04-05 14:39:33 -0400

    add sort lambda

--- lists/ex11_sort-lastname.py
+++ lists/ex11_sort-lastname.py
@@ -6,6 +6,10 @@ from operator import itemgetter, attrgetter
 
 # using ref: call sort by lambda func: https://docs.python.org/3/howto/sorting.html#sortinghowto
 def sort_by_lastnames(data):
+# We can do this by taking advantage of the key parameter to sorted . The value
+# passed to that parameter must be a function that takes a single argument. The func-
+# tion will be invoked once per element, and the function’s return value will be used
+# to sort the values.
     data = sorted(data, key=lambda person: [person["last"], person["first"]])
     print_names(data)
 
@@ -22,9 +26,19 @@ def alphabetize_names(data):
 
 
 people = [
-    {"first": "Reuven", "last": "Lerner", "email": "reuven@lerner.co.il"},
-    {"first": "Donald", "last": "Tod", "email": "donald@lerner.co.il"},
-    {"first": "Vladimir", "last": "Petral", "email": "vladimir@lerner.co.il"}
+    {"first": "ceuven", "last": "Lerner", "email": "reuven@lerner.co.il"},
+    {"first": "bonald", "last": "Tod", "email": "donald@lerner.co.il"},
+    {"first": "aladimir", "last": "Petral", "email": "vladimir@lerner.co.il"}
 ]
 
+# way 1
 alphabetize_names(people)
+
+# way 2
+people.sort(key=lambda x: (x['last'], x['first']), reverse=False)
+print_names(people)
+
+#sort a list:
+people_list = ["a davud", "b jenny", "c tom", "d even"]
+# sory by last name, first name
+print(sorted(people_list, key=lambda person: (person.split()[-1].lower(), person.split()[-2].lower())))
