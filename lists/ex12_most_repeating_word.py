14e4a50b719dafe6763b94e3655118d5b589590b
kangli <li9kang14@gmail.com>
2021-04-12 10:23:05 -0400

    add sort by most repeated letters

---
+++ lists/ex12_most_repeating_word.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+"""Write a function, most_repeating_word, that takes a sequence of strings as input. The
+function should return the string that contains the greatest number of repeated let-
+ters."""
+
+from collections import Counter
+# https://www.youtube.com/watch?v=n_g_uZf65EQ
+# collections is a module and, Counter is a class in the collections module
+
+
+def most_repeating_letters_in_word(word):
+    """Return the count for the most occurences of the leter in given word"""
+
+    return Counter(word).most_common(1)[0][1]
+
+
+def most_repeating_word(words):
+    return max(words, key=most_repeating_letters_in_word)
+
+words = ['this', 'is', 'an', 'elementary', 'test', 'example']
+# most_repeating_word(words)
+# print(most_repeating_letters_in_word("aa1abc"))
+print(most_repeating_word(words))
