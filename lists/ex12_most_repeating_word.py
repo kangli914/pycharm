0256a2f51c0858a0ba11d15b5fb2aa41a2416a17
kangli <kang.li@elasticpath.com>
2021-04-16 10:10:51 -0400

    add sort

--- lists/ex12_most_repeating_word.py
+++ lists/ex12_most_repeating_word.py
@@ -17,8 +17,9 @@ def most_repeating_letters_in_word(word):
 
 def most_repeating_word(words):
     return max(words, key=most_repeating_letters_in_word)
+    #return sorted(words, key=most_repeating_letters_in_word)[-1]
 
-words = ['this', 'is', 'an', 'elementary', 'test', 'example']
+words = ['this', 'iss', 'an', 'elementary', 'test', 'example']
 # most_repeating_word(words)
 # print(most_repeating_letters_in_word("aa1abc"))
 print(most_repeating_word(words))
