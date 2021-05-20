d6b1bf9f36f700e51d3b366c812033c13a2d54d8
kangli <kang.li@elasticpath.com>
2021-05-20 09:43:08 -0400

    update scoop

--- oop/ex35_scoop.py
+++ oop/ex35_scoop.py
@@ -15,6 +15,12 @@ class Scoop:
         return self.flavor
 
 
+# option 1 - create a list of Scoop object first then enumerate them
 list_scoops = [Scoop("chocolate"), Scoop("vanilla"), Scoop("persimmon")]
 for idx, scoop in enumerate(list_scoops, 1):
     print("#{0:1} {1:6}".format(idx, scoop.get_flavor()))
+
+
+# option 2 - use list comprehension to create a list of Scoop object
+for idx, scoop in enumerate([Scoop(flavor) for flavor in ("chocolate", "vanilla", "persimmon")], 1):
+    print("#{0:1} | {1:6}".format(idx, scoop.flavor))
