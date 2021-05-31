f9123020d444b5d1154c0c434fdd8839127c1b1d
kangli <kang.li@elasticpath.com>
2021-05-31 09:39:29 -0400

    object unpack

--- oop/ex38_beverage.py
+++ oop/ex38_beverage.py
@@ -12,5 +12,16 @@ class Beverage:
         self.temperatures = temperatures
 
 
-for i in [Beverage(item[0],item[1]) for item in (("coke", 30), ("water"), ("icec-coffee"))]:
+# without unpacking
+for i in [Beverage(item[0], item[1]) for item in (("coke", 30), ("water"), ("icec-coffee"))]:
     print(i.name, i.temperatures)
+
+
+# tuple unpacking
+for idx, thing in enumerate([Beverage(*item) for item in (("coke", 30), ("water", 50), ("icec-coffee",))], 1):
+    print("#{0} {1:11} {2}".format(idx, thing.name, thing.temperatures))
+
+
+print("*****************")
+for item in (("coke", 30), ("water", 50), ("icec-coffee",)):
+    print(*item)
