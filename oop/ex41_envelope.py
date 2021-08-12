7cfb066d4055872d2b7ff221f9c26e31d1dc4904
kangli <kang.li@elasticpath.com>
2021-08-12 10:36:39 -0400

    oop envelope

--- oop/ex41_envelope.py
+++ oop/ex41_envelope.py
@@ -5,10 +5,13 @@
 class Envelope():
     """Envelope class."""
 
+    # class attribute
+    postage_multiplier = 10
+
     def __init__(self, weight, was_sent=False):
         self.weight = weight
-        self.was_sent = was_sent
         self.postage = 0
+        self.was_sent = was_sent
 
     def send(self):
         if self.postage_needed() <= 0:
@@ -16,30 +19,32 @@ class Envelope():
 
     def add_postage(self, another_postage):
         self.postage += another_postage
-        self.send()
 
     def postage_needed(self):
-        return self.weight*10 - self.postage
-
+        # Uses self.postage_multiplier, rather than Envelope.postage_multiplier, to get the class attribute from the *correct class*
+        return (self.weight*self.postage_multiplier) - self.postage  
 
 class BigEnvelope(Envelope):
     """Inheritance of Envelope class."""
 
-    def __init__(self, weight, was_sent=False):
-        super().__init__(weight, was_sent)
+    # overwrite class attribute
+    postage_multiplier = 15
 
-    def postage_needed(self):
-        return self.weight*15 - self.postage 
+    def __init__(self, weight, was_sent=False):
+        # Implicitly invoking Envelope.__init__ via super()
+        super().__init__(weight, was_sent)
 
 if __name__ == "__main__":
     # base class
     e = Envelope(1)
     e.add_postage(5)
     e.add_postage(4)
+    e.send()
     print(f"small envelope current weight: {e.postage}")
     print(f"small envelope current weight: {e.was_sent}")
 
     e.add_postage(1)
+    e.send()
     print(f"small envelope current weight: {e.postage}")
     print(f"small envelope current weight: {e.was_sent}")
 
@@ -48,5 +53,6 @@ if __name__ == "__main__":
     big.add_postage(5)
     big.add_postage(4)
     big.add_postage(2)
+    e.send()
     print(f"big envelope current weight: {big.postage}")
     print(f"big envelope current weight: {big.was_sent}")
