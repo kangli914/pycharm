ab97eb13835d991f9a20bc3b5f3164b0232ede2a
kangli <kang.li@elasticpath.com>
2021-08-12 09:58:02 -0400

    oop envelope

--- oop/ex41_envelope.py
+++ oop/ex41_envelope.py
@@ -8,18 +8,18 @@ class Envelope():
     def __init__(self, weight, was_sent=False):
         self.weight = weight
         self.was_sent = was_sent
-        self.current_carried_weight = 0
+        self.postage = 0
 
     def send(self):
         if self.postage_needed() <= 0:
             self.was_sent = True
 
     def add_postage(self, another_postage):
-        self.current_carried_weight += another_postage
+        self.postage += another_postage
         self.send()
 
     def postage_needed(self):
-        return self.weight*10 - self.current_carried_weight
+        return self.weight*10 - self.postage
 
 
 class BigEnvelope(Envelope):
@@ -29,18 +29,18 @@ class BigEnvelope(Envelope):
         super().__init__(weight, was_sent)
 
     def postage_needed(self):
-        return self.weight*15 - self.current_carried_weight 
+        return self.weight*15 - self.postage 
 
 if __name__ == "__main__":
     # base class
     e = Envelope(1)
     e.add_postage(5)
     e.add_postage(4)
-    print(f"small envelope current weight: {e.current_carried_weight}")
+    print(f"small envelope current weight: {e.postage}")
     print(f"small envelope current weight: {e.was_sent}")
 
     e.add_postage(1)
-    print(f"small envelope current weight: {e.current_carried_weight}")
+    print(f"small envelope current weight: {e.postage}")
     print(f"small envelope current weight: {e.was_sent}")
 
     # inheritance
@@ -48,5 +48,5 @@ if __name__ == "__main__":
     big.add_postage(5)
     big.add_postage(4)
     big.add_postage(2)
-    print(f"big envelope current weight: {big.current_carried_weight}")
+    print(f"big envelope current weight: {big.postage}")
     print(f"big envelope current weight: {big.was_sent}")
