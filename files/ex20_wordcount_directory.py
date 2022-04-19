8921d8d5b26a1fe71792b86a1c102b9a9de46c1a
kangli <kang.li@elasticpath.com>
2022-04-19 09:22:30 -0400

    word count

--- files/ex20_wordcount_directory.py
+++ files/ex20_wordcount_directory.py
@@ -7,8 +7,12 @@ across all of these files.
 """
 
 import os
+from re import L
 import sys
 from collections import defaultdict
+
+from collections import Counter
+import glob
 
 
 def open_file_safely(file):
@@ -48,16 +52,34 @@ def all_lines(files):
 
 def words_in_line(line, collections):
     """Return a collections of words given a line."""
-    for a_words in line.strip().split():
-        collections[a_words] += 1
+    # for a_words in line.strip().split():
+    #     collections[a_words] += 1
+
+    # use Generator Expressions here!! (look for ex20_words-in-file_generator-expression.py)
+    letters = (letter for a_words in line.strip().split() for letter in a_words)
+    for one_letter in letters:
+        collections[one_letter.lower()] += 1
 
     return collections
+
+
+# another implmentation using glob and counter
+def most_common_letters(dirname):
+    counts = Counter()
+
+    for one_filename in glob.glob(f'{dirname}/*.txt', recursive=True):
+        try:
+            for one_line in open(one_filename):
+                counts.update(one_line.lower())
+        except:
+            pass
+
+    return list(dict(counts.most_common(5)).items())
 
 
 if __name__ == "__main__":
     path = os.getcwd()
     collections = defaultdict(int)
-
     files = all_files(os.path.join(path, "files"))
     for line in all_lines(files):
         # print(line)
@@ -69,3 +91,5 @@ if __name__ == "__main__":
     print("- top 5 -")
     for key in top_five:
         print(f"key: {key}, occurrence: {collections.get(key)}")
+
+    print(most_common_letters(f"{path}/files"))
