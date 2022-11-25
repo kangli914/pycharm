bfe69415f4000b4db7a8d80367d18f582b2f97c1
kangli <kang.li@elasticpath.com>
2022-11-25 10:19:36 -0500

    encript the line

--- files/ex24_encrypt.py
+++ files/ex24_encrypt.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
-"""Rerverse the text in the file. """
+"""encript and decript the files using python build in functions ord()/chr()"""
+
 
 import os
 from pathlib import Path
@@ -11,22 +12,21 @@ def open_file_safely(file, mode="r"):
     try:
         return open(file, mode=mode)
     except OSError:
-        os.error(f"having issue opening the file {file}!")
+        os.error(f"having issue openning the file {file}!")
 
 
 if __name__ == "__main__":
-
-    # /home/kangli/repo/personal/pycharm
-    # root_path = Path.cwd()
-    # /home/kangli/repo/personal/pycharm/files
-    # path = root_path / "files"
-
+    # root = Path.cwd()
     path = Path("files")
     rfile = path / "ex18_vowel.txt"
-    wfile = path / "ex24_out.txt"
+    wfile = path / "ex24_encript.txt"
 
     if rfile.exists():
         with open_file_safely(rfile) as reader, open_file_safely(wfile, mode="w") as writer:
             for line in reader:
-                reversed = line.rstrip()[::-1]
-                writer.write(f"{reversed}\n")
+                newline = None
+                for words in line.split():
+                    for char in words:
+                        # newline += str(ord(char))
+                        # writer.write(line)
+                        writer.write(f'{ord(char)}')
