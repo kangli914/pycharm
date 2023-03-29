4349214dac6e7fdfad4e0392b0e82d34b757f377
kangli <kang.li@elasticpath.com>
2023-03-29 11:03:15 -0400

    update word count with file path set

--- files/ex20_wordcount.py
+++ files/ex20_wordcount.py
@@ -1,52 +1,46 @@
 #!/usr/bin/env python3
 
-"""A simple words count to mimics to the wc to count the charactor, words, unique words and line in the give file."""
+"""
+Write a wordcount function that mimics the wc Unix command.
+The function will take a filename as input and will print four lines of output.
+"""
 
 import os
-import sys
+from pathlib import Path
 
 
-def open_file_safely(file):
-    """Open the file safely.
-
-    Accepts a filename as an argument. It throws the errors and exits
-    if having the error.
-    """
+def open_file_safely(file, mode="r"):
+    """Open file safely."""
     try:
-        return open(file)
+        return open(file, mode=mode)
     except OSError:
-        sys.exit(f"erorr in open the file {file}")
+        os.error("problem opening the file {file}")
+
+
+def count(file):
+    """Return all the count from the file."""
+    count_dict = {"num_char": 0, "num_words": 0, "num_lines": 0, "num_unique_words": 0}
+    unique_words = set()
+    with open_file_safely(file) as f:
+        for line in f:
+            count_dict["num_lines"] += 1
+            count_dict["num_char"] += len(line)
+            # for words in line.strip().split():
+            #     count_dict["num_words"] += 1
+            #     unique_words.update(words)
+
+            # this save another loop compared to above
+            words_list = line.strip().split()
+            count_dict["num_words"] += len(words_list)
+            # set.update take a collection like a list
+            unique_words.update(words_list)
+
+    count_dict["num_unique_words"] = len(unique_words)
+
+    return count_dict
 
 
 if __name__ == "__main__":
-    cwd_path = os.getcwd()
-    print(__file__ + "\n")
-
-    wc_dict = {"lines": 0,
-               "words": 0,
-               "chars": 0}
-
-    unique_wd = set()
-
-    with open_file_safely(os.path.join(cwd_path, "files", "ex20_wordcounts_input.txt")) as f:
-        for ln in f:
-            wc_dict["lines"] += 1
-
-            # To get the number of characters in the current line by calculating len(one_line)
-            # This will count whitespace characters, such as spaces, tabs, and newlines.
-            wc_dict["chars"] += len(ln)
-
-            # str.split() without any arguments, which causes it to use *all* whitespace (=spaces, tabs, and newlines) as delimiters. e.g. the default separator is any whitespace
-
-            # option 1 - not efficient as it iterate the line to find the words
-            # for wd in ln.strip().split():
-            #     wc_dict["words"] += 1
-            #     unique_wd.add(wd)
-
-            # option 2 - more efficient to use line.split() to find the words count, and use set.update(iterable) to find the unique word count:
-            wc_dict["words"] += len(ln.strip().split())
-            unique_wd.update(ln.strip().split())
-
-    wc_dict["unique_word"] = len(unique_wd)
-
-    print(f"lines {wc_dict['lines']}, words {wc_dict['words']}, unique word {wc_dict['unique_word']}, char {wc_dict['chars']}")
+    directory = Path.cwd()
+    file_name = "ex20_wordcounts_input.txt"
+    print(count(directory / file_name))
