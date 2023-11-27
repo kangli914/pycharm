159c9ae26f98a92ac66d299583bda59e786dc295
kang.li <kang.li@elasticpath.com>
2023-11-27 09:45:29 -0500

    add exception doc

--- dicts/pw_14_rainfall.py
+++ dicts/pw_14_rainfall.py
@@ -4,6 +4,14 @@
 
 from collections import defaultdict
 
+"""
+Python exception:
+1. Code that might raise an exception is placed in the try block.
+2. If an exception occurs, the code inside the corresponding except block is executed.
+3. You can have multiple except blocks to handle different types of exceptions.
+4. The else block is optional and contains code that is executed if no exceptions are raised.
+5. The finally block is also optional and contains cleanup code that is always executed, whether an exception occurred or not.
+"""
 
 def get_rainfall():
     city_rain = defaultdict(int)
